112971008697a03628a29f81149b109833e7489a
ericGTEC <128158333+ericDIT@users.noreply.github.com>
2025-12-08 11:02:30 +0100

    Small fixes, SharkGroupDocument variables added (TODO)

--- src/main/java/Group/SharkGroupDocument.java
+++ src/main/java/Group/SharkGroupDocument.java
@@ -1,4 +1,14 @@
 package Group;
 
 public class SharkGroupDocument {
+
+    private byte[] groupId;
+    private byte[] groupCreator;
+    private Currency assignedCurrency;
+    private boolean whitelisted; //Can we still leave it?
+    private boolean encrypted;
+    private boolean balanceVisible;
+    private Object groupDocState; //TODO: we will make an enum
+    private Map<byte[],byte[]> currentMembers; //Signature and ID so I think byte[] is wrong
+    private ArrayList whitelistMember; //Maybe also remove
 }
--- src/main/java/listener/EstablishGroupListner.java
+++ src/main/java/listener/EstablishGroupListner.java
@@ -1,4 +1,4 @@
 package listener;
 
-public class EstablishGroupListner {
+public class EstablishGroupListener {
 }
