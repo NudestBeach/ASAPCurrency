0a951e190e335639b313c26e68ef646d473a900b
ericGTEC <128158333+ericDIT@users.noreply.github.com>
2025-12-08 12:13:48 +0100

    Small fixes

--- src/main/java/Group/SharkGroupDocument.java
+++ src/main/java/Group/SharkGroupDocument.java
@@ -2,10 +2,13 @@ package Group;
 
 public class SharkGroupDocument {
 
+    public static final String DOCUMENT_FORMAT = "application://x-asap-currency-group-document";
+
+
     private byte[] groupId;
     private byte[] groupCreator;
     private Currency assignedCurrency;
-    private boolean whitelisted; //Can we still leave it?
+    private boolean whitelisted;
     private boolean encrypted;
     private boolean balanceVisible;
     private Object groupDocState; //TODO: we will make an enum
