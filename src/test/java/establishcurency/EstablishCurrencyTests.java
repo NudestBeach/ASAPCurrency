44de3c5a991aa9f5191be838421d0d561737fafc
Kacper <124470339+NudestBeach@users.noreply.github.com>
2025-12-10 22:50:43 +0100

    Arbeit am EstablishCurrencyTests

--- src/test/java/establishcurency/EstablishCurrencyTests.java
+++ src/test/java/establishcurency/EstablishCurrencyTests.java
@@ -1,11 +1,16 @@
 package establishcurency;
+import currency.api.SharkCurrency;
+import currency.classes.Currency;
+import currency.classes.LocalCurrency;
 import exepections.ASAPCurrencyException;
 import net.sharksystem.SharkException;
 import net.sharksystem.SharkTestPeerFS;
 import net.sharksystem.asap.ASAPException;
-import net.sharksystem.pki.SharkPKIComponentImpl;
 import net.sharksystem.testhelper.SharkPKITesthelper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static net.sharksystem.utils.testsupport.TestConstants.*;
 
@@ -40,9 +45,32 @@ public class EstablishCurrencyTests {
 
     @Test
     public void aliceCreatesAGroupWithLocalCurrency()
-            throws SharkException, ASAPException, ASAPCurrencyException {
+            throws SharkException, ASAPException, ASAPCurrencyException, IOException {
 
-        //
+        // 1. Alice arranges a new local Currency
+        String currencyName = "AliceTaler";
+        Currency dummyCurrency = new LocalCurrency(
+                false,    // global limit
+                new java.awt.List(),// centralized list (dummy)
+                currencyName,       // Name
+                "A test Currency"  // Spec
+        );
+
+        // 2. Alice creates a new Group using the created Currency
+        // TODO: schlägt fehl, solange Impl leer ist
+        this.aliceComponent.establishGroup(dummyCurrency, false, true);
+
+        // 3. Assert: Check in ASAP Storage, if Channel exists
+        net.sharksystem.asap.ASAPStorage currencyStorage =
+                this.aliceSharkPeer.getASAPStorage(SharkCurrency.CURRENCY_FORMAT);
+
+        // The URI should be the same as defined by the Currency Format
+        CharSequence expetedURI = currencyName;
+
+        // Check if Channel exists in Storage
+        boolean channelExists = currencyStorage.channelExists(expetedURI);
+
+        Assertions.assertTrue(channelExists, "The ASAP-Channel for the currency doesn't exist.");
 
     }
 
